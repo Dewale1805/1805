06e9b144e08ce0d61ae1d2f95ed8b4ea00ce460e
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-14 23:40:57 +0100

    added course, subject and modules to model

--- edulearn/edulearn/settings.py
+++ edulearn/edulearn/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'courses.app.CoursesConfig'
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
