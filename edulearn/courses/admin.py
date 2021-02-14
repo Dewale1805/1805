f7f6142c729ba69fc006b58b16e599bb38fe1754
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-15 00:46:40 +0100

    used fixtures to provide initial data for model

--- edulearn/courses/admin.py
+++ edulearn/courses/admin.py
@@ -7,14 +7,15 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
     prepopulated_fields = {'slug': ('title',)}
 
-class   ModuleInline(admin.StackedInline):
+
+class ModuleInline(admin.StackedInline):
     model = Module
 
 
 @admin.register(Course)
-class   CourseAdmin(admin.ModelAdmin):
+class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'subject', 'created']
     list_filter = ['created', 'subject']
     search_fields = ['title', 'overview']
     prepopulated_fields = {'slug': ('title',)}
-    inlines=ModuleInline
+    inlines = [ModuleInline]
