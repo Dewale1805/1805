41ec9850d85b781f64f1fbd073da05cd3a884132
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-14 02:56:29 +0100

    created subject model

---
+++ edulearn/courses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
