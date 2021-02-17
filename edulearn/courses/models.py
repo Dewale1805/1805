db75975ec649f41c59b540e788405387904d2e17
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-17 01:28:22 +0100

    created custom field OrderField and added to courses models'

--- edulearn/courses/models.py
+++ edulearn/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -40,9 +41,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -59,6 +64,10 @@ class Content(models.Model):
                                      on_delete=models.CASCADE)
     object_id = models.PositiveBigIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
