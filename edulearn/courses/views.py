1cd6425145c3e3772ee1bdd5ab05a1d902f29112
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-18 23:12:29 +0100

    created class based view - ManagecourseView

--- edulearn/courses/views.py
+++ edulearn/courses/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Course
 
-# Create your views here.
+
+class ManageCourseView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
