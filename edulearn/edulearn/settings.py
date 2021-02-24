7d845ec7f36848e9806e72dce3780c28cc351744
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-25 00:50:52 +0100

    created student registration view

--- edulearn/edulearn/settings.py
+++ edulearn/edulearn/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ edulearn/students/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.StudentRegistrationView.as_view(),
+         name='student_registration')
+]
