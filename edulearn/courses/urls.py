6d668fee2eb6a8b1836312a161921c4d506fc465
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-02-24 19:48:34 +0100

    used jquery ui to make the modules and their contents sortable

--- edulearn/courses/urls.py
+++ edulearn/courses/urls.py
@@ -16,5 +16,9 @@ urlpatterns = [
     path('content/<int:id>/delete/', views.ContentDeleteView.as_view(),
          name='module_content_delete'),
     path('module/<int:module_id>/', views.ModuleContentListView.as_view(),
-         name='module_content_list')
+         name='module_content_list'),
+    path('module/order/', views.ModuleOrderView.as_view(),
+         name='module_order'),
+    path('content/order/', views.ContentOrderView.as_view(),
+         name='content_order')
 ]
--- edulearn/courses/views.py
+++ edulearn/courses/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms import modelform_factory
 from django.apps import apps
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
 from .models import Course, Module, Content
 
@@ -146,6 +147,22 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
 
 # class ManageCourseView(ListView):
 #     model = Course
