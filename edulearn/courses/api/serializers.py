bc3347ab4427ab860248f6f47b38fe34cb06f246
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-03-02 02:59:03 +0100

    nested module model in the course model serializer

--- edulearn/courses/api/serializers.py
+++ edulearn/courses/api/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from ..models import Subject
+from ..models import Subject, Course, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['title', 'slug', 'id']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
